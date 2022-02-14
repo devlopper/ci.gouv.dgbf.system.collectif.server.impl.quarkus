312d0cd4d413e97b376f0e978b86608e120e1a26
devlopper <kycdev@gmail.com>
2022-02-14 19:11:28 +0000

    commit

--- src/main/docker/push.bat
+++ src/main/docker/push.bat
@@ -4,6 +4,10 @@ FOR /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do s
 SET fmonth=00%Month%
 SET fday=00%Day%
 SET mydate=%Year%%fmonth:~-2%%fday:~-2%
+
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+set mytime=%mytime: =0%
+
 SET timestamp=v0.0.0-%mydate%%mytime%
 echo Publishing with time stamp : %timestamp% >> push_outputs.txt
 docker tag mic-collectif-api 10.3.4.18:5000/mic-collectif-api:%timestamp% >> push_outputs.txt
