ffaa38d5384d99966d79744fd2fb2cd04233e0f2
devlopper <kycdev@gmail.com>
2022-04-06 13:10:32 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/impl/Configuration.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/impl/Configuration.java
@@ -1,16 +1,13 @@
 package ci.gouv.dgbf.system.collectif.server.impl;
 
-import java.util.concurrent.TimeUnit;
-
 import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithConverter;
-import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
 @StaticInitSafe
 @ConfigMapping(prefix = "collectif")
-public interface Configuration extends org.cyk.quarkus.extension.core_.Configuration {
+public interface Configuration extends org.cyk.quarkus.extension.core_.configuration.Configuration {
 	
 	@WithName("server.client.rest.uri")
 	@WithConverter(StringConverter.class)
@@ -21,40 +18,11 @@ public interface Configuration extends org.cyk.quarkus.extension.core_.Configura
 	Copy copy();
 	/**/
 	
-	public interface Importation {
-		Executor executor();
+	public interface Importation extends org.cyk.quarkus.extension.core_.configuration.Processing {
 		
-		Batch batch();
 	}
 
-	public interface Copy {
-		Executor executor();
+	public interface Copy extends org.cyk.quarkus.extension.core_.configuration.Processing {
 		
-		Batch batch();
-	}
-	
-	public interface Executor {
-		
-		Timeout timeout();
-		
-		public interface Timeout {
-			@WithDefault("5")
-			Long duration();
-			
-			@WithDefault("MINUTES")
-			TimeUnit unit();
-		}
-		
-		Thread thread();
-		
-		public interface Thread {
-			@WithDefault("4")
-			Integer count();
-		}
-	}
-	
-	public interface Batch {
-		@WithDefault("2000")
-		Integer size();
 	}
 }
